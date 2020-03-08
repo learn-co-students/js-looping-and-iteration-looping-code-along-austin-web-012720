a716197a34f12b5dfae6fd5fb6e8e7accde3e9f6
rixong <rixong@gmail.com>
2020-03-08 18:40:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards (name, event) {
+  let result = []
+  for (let i = 0; i < name.length; i++) {
+    result.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+  }
+  return result;
+}
+
+function countDown (num) {
+  let i = num;
+  while (i >= 0) {
+    console.log(i);
+    i -= 1;
+  }
+
+}
