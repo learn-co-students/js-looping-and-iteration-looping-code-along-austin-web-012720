6dafbb32698d17d828a4b6b7f00f10f05eae8836
qudroturkey <reesemb1@gmail.com>
2020-03-09 15:20:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesAry) {
+    var newAry = [];
+    for (let i = 0; i < namesAry.length; i++) {
+        newAry.push(`Thank you, ${namesAry[i]}, for the wonderful surprise gift!`);
+    }
+    return newAry;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--)
+    }
+}
