a5399cc7c7eb6f9de28cb11f56800f028e60df8d
franco-canedo <francanedo1@hotmail.com>
2020-03-09 10:47:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown() {
+    for(let i = 0; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+function writeCards(array) {
+    let new_array = [];
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+    }
+    return new_array;
+}
