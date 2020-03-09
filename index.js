08453d040cd7780bbebf8ebfd254d3b5e7e22369
lughjw <lughjw@gmail.com>
2020-03-09 14:48:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, giftType) {
+    let retArr = [];
+    for (let i = 0; i < names.length; i++) {
+        retArr.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`);
+    }
+
+    return retArr;
+}
+
+function countDown(endingNum) {
+    for(let i = endingNum; i >= 0; i--) {
+        console.log(i);
+    }
+}
